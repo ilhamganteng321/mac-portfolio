1257b2c3572786e5ec02338f8a646f727c7d011e
ilhamganteng321 <ilhamariforpin007tea@gmail.com>
2026-01-10 17:40:43 +0700

    initial project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+import {navIcons, navLinks} from '#constants/index.js';
+
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo"/>
+            </div>
+            <p className="font-bold">Ilham Arip</p>
+            <ul>
+                {navLinks.map(({id, name}) => (
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
+
+            <div>
+                <ul>
+                    {navIcons.map(({id, img}) => (
+                        <li key={id}>
+                            <img src={img} alt={`icon-${id}`} className="icon-hover "/>
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
